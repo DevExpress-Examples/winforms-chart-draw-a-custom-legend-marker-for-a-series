57f9983106f1ba803f3dff616d184e24438461c4
DevExpressExampleBot <fakeEmail>
2020-05-27 17:09:57 +0300

    Source auto update [skip ci]

---
+++ VB/CustomDrawingSample/Model/Employee.vb
@@ -0,0 +1,32 @@
+﻿Imports System.ComponentModel.DataAnnotations.Schema
+Imports System.ComponentModel.DataAnnotations
+Imports System.Collections.Generic
+Imports System
+
+Namespace CustomDrawingSample.Model
+
+	Partial Public Class Employee
+		<Key>
+		<Column("EmployeeID")>
+		Public Property EmployeeId() As Integer
+
+		<Required>
+		<StringLength(20)>
+		Public Property LastName() As String
+
+		<Required>
+		<StringLength(10)>
+		Public Property FirstName() As String
+
+		<Column(TypeName := "image")>
+		Public Property Photo() As Byte()
+
+		Public Overridable Property Orders() As ICollection(Of Order)
+
+		Public ReadOnly Property FullName() As String
+			Get
+				Return String.Format("{0} {1}", FirstName, LastName)
+			End Get
+		End Property
+	End Class
+End Namespace
---
+++ VB/CustomDrawingSample/Model/Order.vb
@@ -0,0 +1,22 @@
+﻿Imports System
+Imports System.ComponentModel.DataAnnotations
+Imports System.ComponentModel.DataAnnotations.Schema
+
+Namespace CustomDrawingSample.Model
+	Partial Public Class Order
+		<Key>
+		Public Property OrderID() As Integer
+
+		<Column("EmployeeID")>
+		<ForeignKey("Employee")>
+		Public Property EmployeeId() As Integer?
+
+		<Column("Freight", TypeName := "smallmoney")>
+		Public Property Freight() As Decimal
+
+		<Column("OrderDate", TypeName := "datetime")>
+		Public Property OrderDate() As DateTime
+
+		Public Overridable Property Employee() As Employee
+	End Class
+End Namespace
