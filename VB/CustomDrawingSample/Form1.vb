4ec6644b2131242d24463bd47ae6056e125de335
DevExpressExampleBot <fakeEmail>
2022-11-24 15:48:13 +0400

    Source auto update [skip ci]

--- VB/CustomDrawingSample/Form1.vb
+++ VB/CustomDrawingSample/Form1.vb
@@ -1,4 +1,4 @@
-﻿Imports CustomDrawingSample.Model
+Imports CustomDrawingSample.Model
 Imports DevExpress.Drawing
 Imports DevExpress.XtraCharts
 Imports System
@@ -9,30 +9,37 @@ Imports System.IO
 Imports System.Linq
 
 Namespace CustomDrawingSample
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits DevExpress.XtraEditors.XtraForm
 
         Private trackedSeriesName As String
-        Private photoCache As New Dictionary(Of String, DXImage)()
+
+        Private photoCache As Dictionary(Of String, DXImage) = New Dictionary(Of String, DXImage)()
 
 #Region "#Constants"
-        Private Const borderSize As Integer = 5
-        Private Const scaledPhotoWidth As Integer = 48
-        Private Const scaledPhotoHeight As Integer = 51
+        Const borderSize As Integer = 5
+
+        Const scaledPhotoWidth As Integer = 48
+
+        Const scaledPhotoHeight As Integer = 51
+
         ' Width and height of scaled photo with border.
-        Private Const totalWidth As Integer = 58
-        Private Const totalHeight As Integer = 61
+        Const totalWidth As Integer = 58
+
+        Const totalHeight As Integer = 61
 
         ' Rects required to create a custom legend series marker.
-        Private Shared ReadOnly photoRect As New Rectangle(borderSize, borderSize, scaledPhotoWidth, scaledPhotoHeight)
-        Private Shared ReadOnly totalRect As New Rectangle(0, 0, totalWidth, totalHeight)
-#End Region
+        Private Shared ReadOnly photoRect As Rectangle = New Rectangle(borderSize, borderSize, scaledPhotoWidth, scaledPhotoHeight)
 
+        Private Shared ReadOnly totalRect As Rectangle = New Rectangle(0, 0, totalWidth, totalHeight)
+
+#End Region
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             Using context = New NwindDbContext()
                 InitPhotoCache(context.Employees)
                 chart.DataSource = context.Orders.ToList()
@@ -41,7 +48,6 @@ Namespace CustomDrawingSample
             chart.SeriesDataMember = "Employee.FullName"
             chart.SeriesTemplate.ArgumentDataMember = "OrderDate"
             chart.SeriesTemplate.ValueDataMembers.AddRange("Freight")
-
             Dim diagram As XYDiagram = TryCast(chart.Diagram, XYDiagram)
             If diagram IsNot Nothing Then
                 diagram.AxisX.DateTimeScaleOptions.AggregateFunction = AggregateFunction.Sum
@@ -59,31 +65,28 @@ Namespace CustomDrawingSample
         Private Sub InitPhotoCache(ByVal employees As IEnumerable(Of Employee))
             photoCache.Clear()
             For Each employee In employees
-                Using stream As New MemoryStream(employee.Photo)
-                    If Not photoCache.ContainsKey(employee.FullName) Then
-                        photoCache.Add(employee.FullName, DXImage.FromStream(stream))
-                    End If
+                Using stream As MemoryStream = New MemoryStream(employee.Photo)
+                    If Not photoCache.ContainsKey(employee.FullName) Then photoCache.Add(employee.FullName, DXImage.FromStream(stream))
                 End Using
-            Next employee
+            Next
         End Sub
 
 #Region "#CustomDrawSeriesImplementation"
         Private Sub OnCustomDrawSeries(ByVal sender As Object, ByVal e As CustomDrawSeriesEventArgs)
             Dim isSelected As Boolean = e.Series.Name.Equals(trackedSeriesName)
             ' Design a series marker image.
-            Dim image As New DXBitmap(totalWidth, totalHeight)
+            Dim image As DXBitmap = New DXBitmap(totalWidth, totalHeight)
             Using graphics As DXGraphics = DXGraphics.FromImage(image)
-                Using fillBrush = If(isSelected, CType(New DXHatchBrush(HatchStyle.DarkDownwardDiagonal, e.LegendDrawOptions.Color, e.LegendDrawOptions.ActualColor2), DXBrush), CType(New DXSolidBrush(e.LegendDrawOptions.Color), DXBrush))
+                Using fillBrush = If(isSelected, CType(New DXHatchBrush(DXHatchStyle.DarkDownwardDiagonal, e.LegendDrawOptions.Color, e.LegendDrawOptions.ActualColor2), DXBrush), CType(New DXSolidBrush(e.LegendDrawOptions.Color), DXBrush))
                     graphics.FillRectangle(fillBrush, totalRect)
                 End Using
-                Dim photo As DXImage = Nothing
-                If photoCache.TryGetValue(e.Series.Name, photo) Then
-                    graphics.DrawImage(photo, photoRect)
-                End If
+
+                Dim photo As DXImage
+                If photoCache.TryGetValue(e.Series.Name, photo) Then graphics.DrawImage(photo, photoRect)
             End Using
+
             e.DXLegendMarkerImage = image
             e.DisposeLegendMarkerImage = True
-
             Dim options As BarDrawOptions = TryCast(e.SeriesDrawOptions, BarDrawOptions)
             If options IsNot Nothing AndAlso isSelected Then
                 options.FillStyle.FillMode = DevExpress.XtraCharts.FillMode.Hatch
