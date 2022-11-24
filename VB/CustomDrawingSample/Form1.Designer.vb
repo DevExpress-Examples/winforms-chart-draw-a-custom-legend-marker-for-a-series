7e851f93d2da4c72f89e67d7831346cb02c86cda
DevExpressExampleBot <fakeEmail>
2022-11-24 16:52:12 +0400

    Source auto update [skip ci]

--- VB/CustomDrawingSample/Form1.Designer.vb
+++ VB/CustomDrawingSample/Form1.Designer.vb
@@ -1,70 +1,70 @@
-﻿Namespace CustomDrawingSample
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace CustomDrawingSample
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Dim chartTitle1 As New DevExpress.XtraCharts.ChartTitle()
-			Me.chart = New DevExpress.XtraCharts.ChartControl()
-			Me.defaultLookAndFeel1 = New DevExpress.LookAndFeel.DefaultLookAndFeel(Me.components)
-			CType(Me.chart, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' chart
-			' 
-			Me.chart.CrosshairOptions.CrosshairLabelMode = DevExpress.XtraCharts.CrosshairLabelMode.ShowForNearestSeries
-			Me.chart.CrosshairOptions.HighlightPoints = False
-			Me.chart.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.chart.Legend.Name = "Default Legend"
-			Me.chart.Legend.TextVisible = False
-			Me.chart.Location = New System.Drawing.Point(0, 0)
-			Me.chart.Margin = New System.Windows.Forms.Padding(4)
-			Me.chart.Name = "chart"
-			Me.chart.SeriesSerializable = New DevExpress.XtraCharts.Series(){}
-			Me.chart.Size = New System.Drawing.Size(911, 445)
-			Me.chart.TabIndex = 1
-			chartTitle1.Text = "Sales Volume by Year"
-			Me.chart.Titles.AddRange(New DevExpress.XtraCharts.ChartTitle() { chartTitle1})
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(7F, 16F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(911, 445)
-			Me.Controls.Add(Me.chart)
-			Me.Margin = New System.Windows.Forms.Padding(4)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.Load += new System.EventHandler(this.Form1_Load);
-			CType(Me.chart, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
-		Private chart As DevExpress.XtraCharts.ChartControl
-		Private defaultLookAndFeel1 As DevExpress.LookAndFeel.DefaultLookAndFeel
-	End Class
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Dim chartTitle1 As DevExpress.XtraCharts.ChartTitle = New DevExpress.XtraCharts.ChartTitle()
+            Me.chart = New DevExpress.XtraCharts.ChartControl()
+            Me.defaultLookAndFeel1 = New DevExpress.LookAndFeel.DefaultLookAndFeel(Me.components)
+            CType((Me.chart), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' chart
+            ' 
+            Me.chart.CrosshairOptions.CrosshairLabelMode = DevExpress.XtraCharts.CrosshairLabelMode.ShowForNearestSeries
+            Me.chart.CrosshairOptions.HighlightPoints = False
+            Me.chart.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.chart.Legend.Name = "Default Legend"
+            Me.chart.Legend.TextVisible = False
+            Me.chart.Location = New System.Drawing.Point(0, 0)
+            Me.chart.Margin = New System.Windows.Forms.Padding(4)
+            Me.chart.Name = "chart"
+            Me.chart.SeriesSerializable = New DevExpress.XtraCharts.Series(-1) {}
+            Me.chart.Size = New System.Drawing.Size(911, 445)
+            Me.chart.TabIndex = 1
+            chartTitle1.Text = "Sales Volume by Year"
+            Me.chart.Titles.AddRange(New DevExpress.XtraCharts.ChartTitle() {chartTitle1})
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(7F, 16F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(911, 445)
+            Me.Controls.Add(Me.chart)
+            Me.Margin = New System.Windows.Forms.Padding(4)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.chart), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
+
+#End Region
+        Private chart As DevExpress.XtraCharts.ChartControl
+
+        Private defaultLookAndFeel1 As DevExpress.LookAndFeel.DefaultLookAndFeel
+    End Class
 End Namespace
-
--- VB/CustomDrawingSample/Form1.vb
+++ VB/CustomDrawingSample/Form1.vb
@@ -1,4 +1,4 @@
-﻿Imports CustomDrawingSample.Model
+Imports CustomDrawingSample.Model
 Imports DevExpress.XtraCharts
 Imports System
 Imports System.Collections.Generic
@@ -8,87 +8,90 @@ Imports System.IO
 Imports System.Linq
 
 Namespace CustomDrawingSample
-	Partial Public Class Form1
-		Inherits DevExpress.XtraEditors.XtraForm
 
-		Private trackedSeriesName As String
-		Private photoCache As New Dictionary(Of String, Image)()
+    Public Partial Class Form1
+        Inherits DevExpress.XtraEditors.XtraForm
 
-		#Region "#Constants"
-		Private Const borderSize As Integer = 5
-		Private Const scaledPhotoWidth As Integer = 48
-		Private Const scaledPhotoHeight As Integer = 51
-		' Width and height of scaled photo with border.
-		Private Const totalWidth As Integer = 58
-		Private Const totalHeight As Integer = 61
+        Private trackedSeriesName As String
 
-		' Rects required to create a custom legend series marker.
-		Private Shared ReadOnly photoRect As New Rectangle(borderSize, borderSize, scaledPhotoWidth, scaledPhotoHeight)
-		Private Shared ReadOnly totalRect As New Rectangle(0, 0, totalWidth, totalHeight)
-		#End Region
+        Private photoCache As Dictionary(Of String, Image) = New Dictionary(Of String, Image)()
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+#Region "#Constants"
+        Const borderSize As Integer = 5
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-			Using context = New NwindDbContext()
-				InitPhotoCache(context.Employees)
-				chart.DataSource = context.Orders.ToList()
-			End Using
+        Const scaledPhotoWidth As Integer = 48
 
-			chart.SeriesDataMember = "Employee.FullName"
-			chart.SeriesTemplate.ArgumentDataMember = "OrderDate"
-			chart.SeriesTemplate.ValueDataMembers.AddRange("Freight")
+        Const scaledPhotoHeight As Integer = 51
 
-			Dim diagram As XYDiagram = TryCast(chart.Diagram, XYDiagram)
-			If diagram IsNot Nothing Then
-				diagram.AxisX.DateTimeScaleOptions.AggregateFunction = AggregateFunction.Sum
-				diagram.AxisX.DateTimeScaleOptions.MeasureUnit = DateTimeMeasureUnit.Year
-			End If
+        ' Width and height of scaled photo with border.
+        Const totalWidth As Integer = 58
 
-			AddHandler chart.CustomDrawSeries, AddressOf OnCustomDrawSeries
-			AddHandler chart.ObjectHotTracked, AddressOf OnObjectHotTracked
-		End Sub
+        Const totalHeight As Integer = 61
 
-		Private Sub OnObjectHotTracked(ByVal sender As Object, ByVal e As HotTrackEventArgs)
-			trackedSeriesName = If(e.HitInfo.InSeries, CType(e.HitInfo.Series, Series).Name, Nothing)
-		End Sub
+        ' Rects required to create a custom legend series marker.
+        Private Shared ReadOnly photoRect As Rectangle = New Rectangle(borderSize, borderSize, scaledPhotoWidth, scaledPhotoHeight)
 
-		Private Sub InitPhotoCache(ByVal employees As IEnumerable(Of Employee))
-			photoCache.Clear()
-			For Each employee In employees
-				Using stream As New MemoryStream(employee.Photo)
-					If Not photoCache.ContainsKey(employee.FullName) Then
-						photoCache.Add(employee.FullName, Image.FromStream(stream))
-					End If
-				End Using
-			Next employee
-		End Sub
+        Private Shared ReadOnly totalRect As Rectangle = New Rectangle(0, 0, totalWidth, totalHeight)
 
-		#Region "#CustomDrawSeriesImplementation"
-		Private Sub OnCustomDrawSeries(ByVal sender As Object, ByVal e As CustomDrawSeriesEventArgs)
-			Dim isSelected As Boolean = e.Series.Name.Equals(trackedSeriesName)
-			' Design a series marker image.
-			Dim image As New Bitmap(totalWidth, totalHeight)
-			Using graphics As Graphics = System.Drawing.Graphics.FromImage(image)
-				Using fillBrush = If(isSelected, CType(New HatchBrush(HatchStyle.DarkDownwardDiagonal, e.LegendDrawOptions.Color, e.LegendDrawOptions.ActualColor2), Brush), CType(New SolidBrush(e.LegendDrawOptions.Color), Brush))
-					graphics.FillRectangle(fillBrush, totalRect)
-				End Using
-				Dim photo As Image = Nothing
-				If photoCache.TryGetValue(e.Series.Name, photo) Then
-					graphics.DrawImage(photo, photoRect)
-				End If
-			End Using
-			e.LegendMarkerImage = image
-			e.DisposeLegendMarkerImage = True
+#End Region
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			Dim options As BarDrawOptions = TryCast(e.SeriesDrawOptions, BarDrawOptions)
-			If options IsNot Nothing AndAlso isSelected Then
-				options.FillStyle.FillMode = DevExpress.XtraCharts.FillMode.Hatch
-				CType(options.FillStyle.Options, HatchFillOptions).HatchStyle = HatchStyle.DarkDownwardDiagonal
-			End If
-		End Sub
-		#End Region
-	End Class
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            Using context = New NwindDbContext()
+                InitPhotoCache(context.Employees)
+                chart.DataSource = context.Orders.ToList()
+            End Using
+
+            chart.SeriesDataMember = "Employee.FullName"
+            chart.SeriesTemplate.ArgumentDataMember = "OrderDate"
+            chart.SeriesTemplate.ValueDataMembers.AddRange("Freight")
+            Dim diagram As XYDiagram = TryCast(chart.Diagram, XYDiagram)
+            If diagram IsNot Nothing Then
+                diagram.AxisX.DateTimeScaleOptions.AggregateFunction = AggregateFunction.Sum
+                diagram.AxisX.DateTimeScaleOptions.MeasureUnit = DateTimeMeasureUnit.Year
+            End If
+
+            AddHandler chart.CustomDrawSeries, AddressOf OnCustomDrawSeries
+            AddHandler chart.ObjectHotTracked, AddressOf OnObjectHotTracked
+        End Sub
+
+        Private Sub OnObjectHotTracked(ByVal sender As Object, ByVal e As HotTrackEventArgs)
+            trackedSeriesName = If(e.HitInfo.InSeries, CType(e.HitInfo.Series, Series).Name, Nothing)
+        End Sub
+
+        Private Sub InitPhotoCache(ByVal employees As IEnumerable(Of Employee))
+            photoCache.Clear()
+            For Each employee In employees
+                Using stream As MemoryStream = New MemoryStream(employee.Photo)
+                    If Not photoCache.ContainsKey(employee.FullName) Then photoCache.Add(employee.FullName, Image.FromStream(stream))
+                End Using
+            Next
+        End Sub
+
+#Region "#CustomDrawSeriesImplementation"
+        Private Sub OnCustomDrawSeries(ByVal sender As Object, ByVal e As CustomDrawSeriesEventArgs)
+            Dim isSelected As Boolean = e.Series.Name.Equals(trackedSeriesName)
+            ' Design a series marker image.
+            Dim image As Bitmap = New Bitmap(totalWidth, totalHeight)
+            Using graphics As Graphics = Graphics.FromImage(image)
+                Using fillBrush = If(isSelected, CType(New HatchBrush(HatchStyle.DarkDownwardDiagonal, e.LegendDrawOptions.Color, e.LegendDrawOptions.ActualColor2), Brush), CType(New SolidBrush(e.LegendDrawOptions.Color), Brush))
+                    graphics.FillRectangle(fillBrush, totalRect)
+                End Using
+
+                Dim photo As Image
+                If photoCache.TryGetValue(e.Series.Name, photo) Then graphics.DrawImage(photo, photoRect)
+            End Using
+
+            e.LegendMarkerImage = image
+            e.DisposeLegendMarkerImage = True
+            Dim options As BarDrawOptions = TryCast(e.SeriesDrawOptions, BarDrawOptions)
+            If options IsNot Nothing AndAlso isSelected Then
+                options.FillStyle.FillMode = DevExpress.XtraCharts.FillMode.Hatch
+                CType(options.FillStyle.Options, HatchFillOptions).HatchStyle = HatchStyle.DarkDownwardDiagonal
+            End If
+        End Sub
+#End Region
+    End Class
 End Namespace
